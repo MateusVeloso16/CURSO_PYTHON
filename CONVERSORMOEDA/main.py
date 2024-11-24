557b427e3de6ca91bf362cacf1a75aae8824af59
Mateus Veloso <mateus.16.veloso@gmail.com>
2024-11-24 23:12:44 +0000

    Import XML file in dic. format. Creating functions for money conversion.

--- CONVERSORMOEDA/main.py
+++ CONVERSORMOEDA/main.py
@@ -1,5 +1,5 @@
 import customtkinter
-from pegar_moedas import nomes_moedas
+from pegar_moedas import nomes_moedas, conversoes_disponiveis
 
 customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("dark-blue")
@@ -7,12 +7,20 @@ customtkinter.set_default_color_theme("dark-blue")
 janela = customtkinter.CTk()
 janela.geometry("500x500")
 
+dic_conversoes_disponiveis = conversoes_disponiveis()
+
 titulo = customtkinter.CTkLabel(janela, text = "Conversor de Moedas", font=("Arial",25))
 texto_moeda_origem = customtkinter.CTkLabel(janela, text = "Selecione a moeda de origem")
 texto_moeda_destino = customtkinter.CTkLabel(janela, text = "Selecione a moeda de destino")
 
-campo_moeda_origem = customtkinter.CTkOptionMenu(janela, values=["USD", "BRL", "EUR", "BTC"])
-campo_moeda_destino = customtkinter.CTkOptionMenu(janela, values=["USD", "BRL", "EUR", "BTC"])
+
+def carregar_moedas_destino(moeda_selecionada):
+    lista_moedas_destino = dic_conversoes_disponiveis[moeda_selecionada]
+    campo_moeda_destino.configure(values=lista_moedas_destino)
+    campo_moeda_destino.set(lista_moedas_destino[0])
+
+campo_moeda_origem = customtkinter.CTkOptionMenu(janela, values=list(dic_conversoes_disponiveis.keys()), command=carregar_moedas_destino)
+campo_moeda_destino = customtkinter.CTkOptionMenu(janela, values=["Selecione uma moeda de origem."])
 
 def converter_moeda():
     print("Converter moeda")
--- CONVERSORMOEDA/pegar_moedas.py
+++ CONVERSORMOEDA/pegar_moedas.py
@@ -7,8 +7,19 @@ def nomes_moedas():
     moedas = dic_moedas["xml"]
     return moedas
 
-with open("CONVERSORMOEDA\conversoes.xml", "rb") as arquivo_conversoes:
-    dic_conversoes = xmltodict.parse(arquivo_conversoes)
 
-conversoes = dic_conversoes["xml"]
-print(dic_conversoes)
+def conversoes_disponiveis():
+    with open("CONVERSORMOEDA\conversoes.xml", "rb") as arquivo_conversoes:
+        dic_conversoes = xmltodict.parse(arquivo_conversoes)
+
+    conversoes = dic_conversoes["xml"]
+    dic_conversoes_disponiveis = {}
+    for par_conversao in conversoes:
+        moeda_origem, moeda_destino = par_conversao.split("-")
+        if moeda_origem in dic_conversoes_disponiveis:
+                dic_conversoes_disponiveis[moeda_origem].append(moeda_destino)
+        else:
+                dic_conversoes_disponiveis[moeda_origem] = [moeda_destino]
+    return dic_conversoes_disponiveis
+
+conversoes_disponiveis()
