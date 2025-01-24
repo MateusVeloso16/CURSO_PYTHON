7a5dcd016fd9181214a0a93efda75560a9177390
MateusVeloso16 <mateus.16.veloso@gmail.com>
2025-01-24 17:42:19 +0000

    Updates valaidarstring exercise.

--- CONDITIONALEXERCISES/validarstring.py
+++ CONDITIONALEXERCISES/validarstring.py
@@ -1,16 +1,18 @@
 numero_telefone = input("Informe o numero de telefone: ")
-print(numero_telefone)
 
 
 numero_telefone = numero_telefone.strip()
-print(numero_telefone)
 
 len_telefone = len(numero_telefone)
 
+if "-" in numero_telefone:
+    numero_telefone = numero_telefone.replace("-", (""))
+
 if len_telefone == 7:
     numero_telefone = "3" + numero_telefone
-    print(numero_telefone)
 elif len_telefone == 8:
     print("Numero digitado corretamente. ")
 else:
     print("Numero invalido. Tente novamente. ")
+
+print(numero_telefone)
